7e4f1ae547d1e2946a67b79e2e34b33a34683cd6
Matthias Lücke <ml@backslash-n.com>
2024-09-10 23:46:57 +0200

    added path checks on startup

--- docker/00-setup.sh
+++ docker/00-setup.sh
@@ -2,6 +2,14 @@
 
 cd /var/www/html || exit
 
+echo "ensuring folders exist"
+runuser -u web -- mkdir -p storage/logs/
+runuser -u web -- mkdir -p storage/app/public
+runuser -u web -- mkdir -p storage/framework/cache/data
+runuser -u web -- mkdir -p storage/framework/sessions
+runuser -u web -- mkdir -p storage/framework/views
+runuser -u web -- mkdir -p storage/framework/statamic
+
 runuser -u web -- php artisan optimize
 runuser -u web -- php artisan event:cache
 runuser -u web -- php artisan view:cache

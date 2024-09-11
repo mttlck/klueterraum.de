5baf067af2c087b79b1b6cf80105ba1d3027d642
Matthias Lücke <ml@backslash-n.com>
2024-09-11 14:17:43 +0200

    run/setup script update

--- docker/00-setup.sh
+++ docker/00-setup.sh
@@ -10,6 +10,15 @@ runuser -u web -- mkdir -p storage/framework/sessions
 runuser -u web -- mkdir -p storage/framework/views
 runuser -u web -- mkdir -p storage/framework/statamic
 
+runuser -u web -- mkdir -p storage/content/assets
+runuser -u web -- mkdir -p storage/content/collections
+runuser -u web -- mkdir -p storage/content/globals
+runuser -u web -- mkdir -p storage/content/navigation
+runuser -u web -- mkdir -p storage/content/taxonomies
+runuser -u web -- mkdir -p storage/content/trees
+
+#todo: copy initial content for preview deployments
+
 runuser -u web -- php artisan optimize
 runuser -u web -- php artisan event:cache
 runuser -u web -- php artisan view:cache

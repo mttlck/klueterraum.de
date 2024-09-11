27c04af60052c3b80dd15ba75dfbe255e99f6a78
Matthias Lücke <ml@backslash-n.com>
2024-09-11 14:59:06 +0200

    hosting updates

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: [
+            '172.22.0.0/8',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- docker/00-setup.sh
+++ docker/00-setup.sh
@@ -2,23 +2,6 @@
 
 cd /var/www/html || exit
 
-echo "ensuring folders exist"
-runuser -u web -- mkdir -p storage/logs/
-runuser -u web -- mkdir -p storage/app/public
-runuser -u web -- mkdir -p storage/framework/cache/data
-runuser -u web -- mkdir -p storage/framework/sessions
-runuser -u web -- mkdir -p storage/framework/views
-runuser -u web -- mkdir -p storage/framework/statamic
-
-runuser -u web -- mkdir -p storage/content/assets
-runuser -u web -- mkdir -p storage/content/collections
-runuser -u web -- mkdir -p storage/content/globals
-runuser -u web -- mkdir -p storage/content/navigation
-runuser -u web -- mkdir -p storage/content/taxonomies
-runuser -u web -- mkdir -p storage/content/trees
-
-#todo: copy initial content for preview deployments
-
 runuser -u web -- php artisan optimize
 runuser -u web -- php artisan event:cache
 runuser -u web -- php artisan view:cache
